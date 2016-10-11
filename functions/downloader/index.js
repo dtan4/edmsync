61dd17557c92d5aee37df8f8f6b998f671a01e7f
Daisuke Fujita <dtanshi45@gmail.com>
2016-10-12 01:30:16 +0900

    Remove currentRevision now

--- functions/downloader/index.js
+++ functions/downloader/index.js
@@ -4,7 +4,6 @@ let Dropbox = require('dropbox');
 
 exports.handle = (event, context, callback) => {
   let path = event.path;
-  let currentRevision = event.revision;
   let client = new Dropbox({ accessToken: process.env.DROPBOX_ACCESS_TOKEN });
   let promise = client.filesDownload({ path: path });
 
